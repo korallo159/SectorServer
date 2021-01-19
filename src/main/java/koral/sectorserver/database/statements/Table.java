598119a41282f92716232db337a7a01fb596a4d7
koral <orszulak.k@o2.pl>
2021-01-19 04:59:12 +0100

    hikari library data source prepare for future

---
+++ src/main/java/koral/sectorserver/database/statements/Table.java
@@ -0,0 +1,12 @@
+package koral.sectorserver.database.statements;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Table {
+
+    public static void createDefaultTable(){
+        Connection connection = null;
+        PreparedStatement statement = null;
+    }
+}
